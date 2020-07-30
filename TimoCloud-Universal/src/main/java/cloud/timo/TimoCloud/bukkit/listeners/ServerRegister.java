f472f800d84415f0ef6a5d994a7f70ed30ec8165
meerpaluten <dev@meerpaluten.de>
2020-07-30 22:48:30 +0200

    Add ServerRegister#onServerUnregister

--- TimoCloud-Universal/src/main/java/cloud/timo/TimoCloud/bukkit/listeners/ServerRegister.java
+++ TimoCloud-Universal/src/main/java/cloud/timo/TimoCloud/bukkit/listeners/ServerRegister.java
@@ -3,6 +3,7 @@ package cloud.timo.TimoCloud.bukkit.listeners;
 import cloud.timo.TimoCloud.api.events.EventHandler;
 import cloud.timo.TimoCloud.api.events.Listener;
 import cloud.timo.TimoCloud.api.events.server.ServerRegisterEvent;
+import cloud.timo.TimoCloud.api.events.server.ServerUnregisterEvent;
 import cloud.timo.TimoCloud.bukkit.TimoCloudBukkit;
 
 public class ServerRegister implements Listener {
@@ -11,5 +12,11 @@ public class ServerRegister implements Listener {
     public void onServerRegister(ServerRegisterEvent event) {
         if (!event.getServer().getId().equals(TimoCloudBukkit.getInstance().getServerId())) return;
         TimoCloudBukkit.getInstance().setServerRegistered(true);
+    }
+
+    @EventHandler
+    public void onServerUnregister(ServerUnregisterEvent event) {
+        if (!event.getServer().getId().equals(TimoCloudBukkit.getInstance().getServerId())) return;
+        TimoCloudBukkit.getInstance().setServerRegistered(false);
     }
 }
