0c4988a4eb3f3a342dd40d6ec13554faadd30c99
DerMagsch <49600333+DerMagsch@users.noreply.github.com>
2021-07-08 20:43:40 +0200

    Fix InaccessibleObjectException on Server start

--- TimoCloud-Universal/src/main/java/cloud/timo/TimoCloud/base/managers/BaseInstanceManager.java
+++ TimoCloud-Universal/src/main/java/cloud/timo/TimoCloud/base/managers/BaseInstanceManager.java
@@ -295,6 +295,7 @@ public class BaseInstanceManager {
                                 " -Dtimocloud-static=" + server.isStatic() +
                                 " -Dtimocloud-templatedirectory=" + templateDirectory.getAbsolutePath() +
                                 " -Dtimocloud-temporarydirectory=" + temporaryDirectory.getAbsolutePath() +
+                                " --add-opens java.base/java.lang=ALL-UNNAMED " +
                                 " -jar spigot.jar -p " + serverPort + " " + buildStartParameters(server.getSpigotParameters()) +
                                 "'"
                 ).start();
@@ -504,6 +505,7 @@ public class BaseInstanceManager {
                                 " -Dtimocloud-static=" + proxy.isStatic() +
                                 " -Dtimocloud-templatedirectory=" + templateDirectory.getAbsolutePath() +
                                 " -Dtimocloud-temporarydirectory=" + temporaryDirectory.getAbsolutePath() +
+                                " --add-opens java.base/java.lang=ALL-UNNAMED " +
                                 " -jar " + fileName +
                                 "'"
                 ).start();
