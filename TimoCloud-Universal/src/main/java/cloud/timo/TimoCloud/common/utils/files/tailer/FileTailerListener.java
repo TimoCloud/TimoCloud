093a2086464dec3f913194cb7da9f64a7fbf2456
VytskaLT <VytskaLT@protonmail.com>
2021-07-25 12:50:16 +0300

    fix again

--- TimoCloud-Universal/src/main/java/cloud/timo/TimoCloud/common/utils/files/tailer/FileTailerListener.java
+++ TimoCloud-Universal/src/main/java/cloud/timo/TimoCloud/common/utils/files/tailer/FileTailerListener.java
@@ -2,19 +2,11 @@ package cloud.timo.TimoCloud.common.utils.files.tailer;
 
 public abstract class FileTailerListener {
 
-    /**
-     * Supposed to be overridden
-     *
-     * @param line Line
-     */
     public void readLine(String line) {
+        // Supposed to be overridden
     }
 
-    /**
-     * Supposed to be overridden
-     *
-     * @param e Exception
-     */
     public void handleException(Exception e) {
+        // Supposed to be overridden
     }
 }
