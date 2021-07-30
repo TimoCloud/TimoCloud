32e933a522885a77e00992a83b74db01db3d41ab
meerpaluten <45709417+meerpaluten@users.noreply.github.com>
2021-07-30 20:43:10 +0200

    import org.bukkit.Bukkit;

--- TimoCloud-Universal/src/main/java/cloud/timo/TimoCloud/bukkit/managers/SignManager.java
+++ TimoCloud-Universal/src/main/java/cloud/timo/TimoCloud/bukkit/managers/SignManager.java
@@ -20,6 +20,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 
 import java.util.*;
 import java.util.stream.Collectors;
